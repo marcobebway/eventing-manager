7f14c231682b571d902df1f5bc39d79d4ae8ab15
marcobebway <marco.bebway@sap.com>
2022-11-25 22:38:11 +0100

    save progress - try to deploy eventing charts

--- controllers/keda_controller.go
+++ controllers/keda_controller.go
@@ -56,12 +56,10 @@ type KedaReconciler struct {
 //+kubebuilder:rbac:groups=apiextensions.k8s.io,resources=customresourcedefinitions,verbs=create;delete;update;patch
 //+kubebuilder:rbac:groups="*",resources="*/scale",verbs="*"
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=list;watch;create;delete;update;patch
-//+kubebuilder:rbac:groups=apps,resources=statefulsets;replicasets,verbs=list;watch
+//+kubebuilder:rbac:groups=apps,resources=statefulsets;replicasets,verbs=list;watch;create;delete;update;patch
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs="*"
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs="*"
-//+kubebuilder:rbac:groups="keda.sh",resources=clustertriggerauthentications;clustertriggerauthentications/status;scaledjobs;scaledjobs/finalizers;scaledjobs/status;scaledobjects;scaledobjects/finalizers;scaledobjects/status;triggerauthentications;triggerauthentications/status,verbs="*"
 //+kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs="*"
-
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kedas,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kedas/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=operator.kyma-project.io,resources=kedas/finalizers,verbs=update;patch
